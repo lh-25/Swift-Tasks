7f0a0d1244e2dbd2660d20ef4973ae2c57bb9d35
lh-25 <sprinklecake25@gmail.com>
2024-10-29 22:06:38 -0500

    finished UI setup

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,20 @@
 import { TodoCard } from "./TodoCard";
 
-export  function TodoList () {
+export  function TodoList (props) {
+const {todos} = props
+
+
+const tab = 'All'
+
+const filtersTodoList = tab === 'All' ? todos : tab === 'Complete' ? todos.filter(val => val.complete) : todos.filter(val => !val.complete)
   return (
-    <div>
-      <TodoCard />
-    </div>
+    <>
+     
+      {filtersTodoList.map((todo, todoIndex) => {
+        return (
+          <TodoCard key={todoIndex} todoIndex={todoIndex} {...props} />
+        )
+      })}
+    </>
   )
 }
